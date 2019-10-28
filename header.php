f2b022db65f5d6fdbafb22b2a6134ccef75086f4
Vinicius Garcia <vinigarcia87@gmail.com>
2019-10-28 15:03:52 -0300

    - Removendo description do site no lugar errado

--- header.php
+++ header.php
@@ -38,12 +38,6 @@ get_template_part( 'template-parts/head' ); ?>
                 if ( is_front_page() && is_home() ) : ?>
                     <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
                 <?php
-                endif;
-
-                $description = get_bloginfo( 'description', 'display' );
-                if ( $description || is_customize_preview() ) : ?>
-                    <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-                <?php
                 endif; ?>
             </div>
         </div>
