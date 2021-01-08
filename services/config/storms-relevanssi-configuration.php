6715a686879051ce2d041b93299dd2eec7a42561
Vinicius Garcia <vinigarcia87@gmail.com>
2021-01-08 13:17:34 -0300

    - Configurations for Relevanssi and REST API Log plugins

---
+++ services/config/storms-relevanssi-configuration.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Storms Websolutions (http://storms.com.br/)
+ *
+ * @author    Vinicius Garcia | vinicius.garcia@storms.com.br
+ * @copyright (c) Copyright 2012-2020, Storms Websolutions
+ * @license   GPLv2 - GNU General Public License v2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @package   Storms
+ * @version   4.0.0
+ *
+ * Storms relevanssi Configuration file
+ * General customizations for Relevanssi
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if( \StormsFramework\Helper::is_plugin_activated( 'relevanssi/relevanssi.php' ) ) {
+
+	if( ! function_exists( 'storms_define_relevanssi_options' ) ) {
+
+		// Define Relevanssi options
+		function storms_define_relevanssi_options() {
+
+			// Only setup if user is an admin
+			if ( ! current_user_can( 'manage_options' ) ) {
+				return;
+			}
+
+			update_option( 'relevanssi_index_post_types', array( 'product', 'product_variation', /* 'post', 'page' */ ) );
+			update_option( 'relevanssi_index_taxonomies_list', array( 'product_cat', 'product_tag' ) );
+			update_option( 'relevanssi_index_fields', '_sku, _product_attributes' );
+			update_option( 'relevanssi_index_author', 'off' );
+			update_option( 'relevanssi_index_excerpt', 'on' );
+			update_option( 'relevanssi_punctuation', array( 'quotes' => 'replace', 'hyphens' => 'replace', 'ampersands' => 'replace', 'decimals' => 'remove', ) );
+			update_option( 'relevanssi_post_type_weights', array( 'post_tag' => 1, 'category' => 1, ) );
+			update_option( 'relevanssi_log_queries', 'on' );
+			update_option( 'relevanssi_log_queries_with_ip', 'on' );
+			update_option( 'relevanssi_trim_logs', 90 );
+			update_option( 'relevanssi_excerpts', 'off' );
+		}
+		add_action( 'admin_init', 'storms_define_relevanssi_options' );
+
+	}
+
+}
---
+++ services/config/storms-rest-api-log-configuration.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Storms Websolutions (http://storms.com.br/)
+ *
+ * @author    Vinicius Garcia | vinicius.garcia@storms.com.br
+ * @copyright (c) Copyright 2012-2020, Storms Websolutions
+ * @license   GPLv2 - GNU General Public License v2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @package   Storms
+ * @version   4.0.0
+ *
+ * Storms REST API Log Configuration file
+ * General customizations for REST API Log
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if( \StormsFramework\Helper::is_plugin_activated( 'wp-rest-api-log/wp-rest-api-log.php' ) ) {
+
+	if( ! function_exists( 'storms_define_rest_api_log_options' ) ) {
+
+		// Define REST API Log options
+		function storms_define_rest_api_log_options() {
+
+			// Only setup if user is an admin
+			if ( ! current_user_can( 'manage_options' ) ) {
+				return;
+			}
+
+			update_option('wp-rest-api-log-settings-general', array(
+				'logging-enabled' 	 => '1',
+				'purge-days' 		 => '90',
+				'ip-address-display' => 'http_x_forwarded_for'
+			) );
+		}
+		add_action( 'admin_init', 'storms_define_rest_api_log_options' );
+
+	}
+
+}
